298a18467f78df0e46a66fdaa3ca05307e0bf3ac
bibashmanjusubedi <acharabibash@outlook.com>
2025-08-26 14:20:46 +0545

    Sun/Moon Icon for light and dark mode

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const toggleBtn = document.getElementById("toggleTheme");
+const body = document.body;
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+     // Change icon based on mode
+     if (body.classList.contains("dark-mode")) {
+        toggleTheme.classList.remove("fa-moon");
+        toggleTheme.classList.add("fa-sun");
+    } else {
+        toggleTheme.classList.remove("fa-sun");
+        toggleTheme.classList.add("fa-moon");
+    }
 });
